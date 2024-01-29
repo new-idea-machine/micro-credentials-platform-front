4e794f0db22cd68e3f80e9bb83f82d80f48ec1bf
Kevin Woodman <kwoodman1970@yahoo.ca>
2024-01-29 15:09:34 -0700

    Added validation to form for registering a new learner; added luddite-friendly error messages; improved fault-tolerance

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import validator from "validator";
+
 import './App.css'
 
 const serverURL = import.meta.env.VITE_SERVER_URL_ROOT;
@@ -17,27 +19,57 @@ async function submit(submitEvent) {
     isInstructor:  false
   };
 
-  const options =  {
-    method: "POST",
-    mode: "cors",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify(data)
+  let dataIsValid = true;
+
+  if (data.userInfo.name == "") {
+    dataIsValid = false;
+
+    window.alert("\"Name\" is required.");
   }
 
-  try {
-    const response = await fetch(`${serverURL}/user`, options);
-    const result = await response.json();
+  // if (!emailValidator.test(data.userInfo.email)) {
+  if (!validator.isEmail(data.userInfo.email)) {
+    dataIsValid = false;
 
-    if (response.ok) {
-      window.alert(`Registration succeeded.\n\n"${data.userInfo.name}" is user "${result.userUID}".`);
-    }
-    else {
-      window.alert(`Registration failed.\n\nReason:  ${response.status} -- ${response.statusText}`);
-    }
+    window.alert("That's not a valid e-mail address!  Who're you trying to kid?");
   }
-  catch(error)
-  {
-    window.alert(`Registration failed.\n\nReason:  ${error}`);
+
+  if (data.password == "") {
+    dataIsValid = false;
+
+    window.alert("You call that a password?  My mother can pick a better password than that!");
+  }
+
+  if (dataIsValid) {
+    const options =  {
+      method: "POST",
+      mode: "cors",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(data)
+    }
+
+    try {
+      const response = await fetch(`${serverURL}/user`, options);
+      const result = (response?.bodyUsed ? await response?.json() : null);
+
+      console.log(`New user id is ${result?.userUID}`);
+
+      if (response.ok) {
+        window.alert(`Registration succeeded.\n\n"${data.userInfo.name}" has been added to the database.`);
+      }
+      else if (response.status == 403) {
+        window.alert("Registration failed.\n\nA user with these login credentials is already registered.");
+      }
+      else if (response.status < 500) {
+        window.alert("Registration failed.\n\nThe server didn't like these login credentials.");
+      }
+      else {
+        window.alert("Registration failed.\n\nThe server couldn't access the database.");
+      }
+    }
+    catch(error) {
+      window.alert(`Registration failed.\n\nThe server could not be accessed.`);
+    }
   }
 
   return;
