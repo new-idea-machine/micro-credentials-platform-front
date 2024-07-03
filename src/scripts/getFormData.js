2dbdeb978ce8ff4f8b323b68582bd2f0445567d8
Kevin Woodman <kwoodman1970@yahoo.ca>
2024-07-02 22:11:01 -0600

    Fixed getFormData.js by documenting the function's limitations, adding workarounds for browser deficiencies and correcting a typo

--- src/scripts/getFormData.js
+++ src/scripts/getFormData.js
@@ -18,17 +18,46 @@ function getFormData(formElement) {
   isn't a valid date or time).  A number control's will be a number (possibly "NaN").  A
   multi-select control's will be an array of strings.  A checkbox's will be a boolean.  All
   others' will be strings.
+
+  IMPORTANT NOTE:  As of this writing, not all input types are supported by all browsers.  For
+  example, the Mozilla Gecko browser engine currently doesn't support the "week" or "month"
+  input types.  In these cases, the input type in the DOM will be "text" and the corresponding
+  member in the returned object will be a string.
   */
 
   const formData = {};
   const dateTypes = ["date", "datetime", "datetime-local", "month", "time", "week"];
 
   for (const control of formElement.elements) {
-    if (("name" in control) && (control.name !== "")) {
-      if (dateTypes.includes(control.type)) formData[control.name] = control.valueAsDate;
-      else if (control.type === "number") formData[control.name] = control.valueAsNumber;
+    if ("name" in control && control.name !== "") {
+      if (dateTypes.includes(control.type)) {
+        /*
+        WORKAROUND:  As of this writing, the ".valueAsDate" member of date & time input
+        elements is not implemented consistently (or even correctly) across browser engines and
+        is therefore unreliable.  The "Date" object is therefore parsed from the ".value"
+        member of these elements.
+
+        The lone exception here is the "week" type -- there's no other easy way to calculate
+        the date.
+        */
+
+        if (control.type === "week") formData[control.name] = control.valueAsDate;
+        else {
+          /*
+          "time" input type values need to be prefixed with the date of the start of the epoch
+          in order to be parsed correctly.
+          */
+          const epochStart = "1970-01-01T";
+
+          formData[control.name] = new Date(
+            (control.type === "time" ? epochStart : "") + control.value
+          );
+
+          if (isNaN(formData[control.name].valueOf())) formData[control.name] = null;
+        }
+      } else if (control.type === "number") formData[control.name] = control.valueAsNumber;
       else if (control.type === "checkbox") formData[control.name] = control.checked;
-      else if (control.type === "select-multi") {
+      else if (control.type === "select-multiple") {
         formData[control.name] = [];
 
         for (const option of control.options)
